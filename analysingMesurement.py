4309dac447df60ed8ab3bb2733b23e66c4a4e5c0
kanell <maltegerber@gmail.com>
2014-11-06 18:42:02 +0100

    some cleaning up the code and rms is now working

--- analysingMesurement.py
+++ analysingMesurement.py
@@ -13,64 +13,84 @@ import os
 import glob
 import Picoscope2000, Picoscope4000
 import time
+import mpmath
 import math
 
-NPYLENGTH = 500000
-N = 2**16 #Samplepoints
 
-#def FFT_output(measurepoints):
-#    FFTmeasurepoints = np.fft.fftshift(np.fft.fft(measurepoints))
-#    return FFTmeasurepoints
+SAMPLEPOINTS = 2**22 #Samplepoints
+
+
+#class Measurement:
+    
+def load_measurepoints(SAMPLEPOINTS):
+    #Load files from the directory        
+    npyfiles = glob.glob('C:\Users\Malte Gerber\Desktop\PQdata\*.npy')
+    measurepoints = np.load(npyfiles[0])
+    #add the content from all files to one list
+    i = 0
+    while len(measurepoints) < SAMPLEPOINTS:
+        measurepoints = np.concatenate([measurepoints, (np.load(npyfiles[i]))], axis=0) 
+        i = i+1
+    return measurepoints
+    
+def fast_fourier_transformation(measurepoints,SAMPLEPOINTS):
+    #berechnen der Fouriertransformation        
+    FFTmeasurepoints = np.fft.fftshift(np.fft.fft(measurepoints))/SAMPLEPOINTS 
+    #wegkürzen der negativen frequenzen und dafür verdoppeln der Amplituden
+    FFTmeasurepoints = np.absolute(FFTmeasurepoints[len(FFTmeasurepoints)/2.0:]*2.0)   
+    return FFTmeasurepoints  
+    
+def calculate_rms(measurepoints):
+    rms = np.sqrt(np.mean(np.power(np.int32(measurepoints), 2)))
+    return rms
+
+def example_sin_wave(SAMPLEPOINTS):
+    Tn = 0.02 #ms Frequenz der sinusspannung
+    t = np.linspace(0.0,0.2,num=SAMPLEPOINTS) #Zeit für 10 Perioden
+    measurepoints = 230*np.sqrt(2)*np.sin(2*np.pi/Tn*t)
+    return measurepoints
+    
+def plt_definition_sin_wave(FFTmeasurepoints, SAMPLEPOINTS):
+    fs = 327680.0     #Samples/s Abtastrate        
+    deltaT = 1/fs   #s ->Zeit von einem Messpunkt zum nächsten
+    fMax = 1/deltaT   #Hz -> Maximale Frequenz der Fourieranalyse
+    fAxis = np.linspace(0,fMax/2,SAMPLEPOINTS/2)
+    plt.plot(fAxis,FFTmeasurepoints)
+    plt.xlabel("f in Hz")
+    plt.ylabel("FFT")
+    plt.xlim([0,2000])
+
 
 if __name__ == '__main__':
     try:
-        #Fourier Analysis:
-        npyfiles = glob.glob('C:\Users\Malte Gerber\Desktop\PQdata\*.npy')
-        measurepoints = np.load(npyfiles[0])
-
-        i = 0
-        while len(measurepoints) < NPYLENGTH:
-            measurepoints = np.concatenate([measurepoints, (np.load(npyfiles[i]))], axis=0) 
-            i = i+1
-        measurepoints = measurepoints[:N]
-        #must be modified for more measurepoints -> brauch bei vielen Messwerten aber zu lange   
-
+        #Fourier Analysis:    
+        measurement = load_measurepoints(SAMPLEPOINTS)
+        measurement = measurement[:SAMPLEPOINTS]
+        
         #example for the fourier analysis
-             
-#        Tn = 0.02 #ms Frequenz der sinusspannung
-#        t = np.linspace(0.0,0.2,num=N) #Zeit für 10 Perioden
-#        measurepoints = 230*np.sqrt(2)*np.sin(2*np.pi/Tn*t)
-     
+        #measurement = example_sin_wave(SAMPLEPOINTS)    
+
         #FFT: Measured data are given in FFT-equation        
-        FFTmeasurepoints = np.fft.fftshift(np.fft.fft(measurepoints))/N     #berechnen der Fouriertransformation 
-        FFTmeasurepoints = np.absolute(FFTmeasurepoints[len(FFTmeasurepoints)/2.0:]*2.0)    #wegkürzen der negativen frequenzen und dafür verdoppeln der Amplituden
-              
+        FFTmeasurepoints = fast_fourier_transformation(measurement, SAMPLEPOINTS)
         
         #Data for plot mit beispielwerten werten
-#        fs = 327680.0     #Samples/s Abtastrate        
-#        deltaT = 1/fs   #s ->Zeit von einem Messpunkt zum nächsten
-#        fMax = 1/deltaT   #Hz -> Maximale Frequenz der Fourieranalyse
-#        fAxis = np.linspace(0,fMax/2,N/2)
-#        plt.plot(fAxis,FFTmeasurepoints)
-#        plt.xlabel("f in Hz")
-#        plt.ylabel("FFT")
-#        plt.xlim([0,2000])
-        
+        #plt_definition_sin_wave(measurement, SAMPLEPOINTS)
         
         #Data for plot mit echten werten
         deltaT = 0.002      #ms
         fMax = 1.0/deltaT   #KHz
-        fAxis = np.linspace(0,fMax/2,N/2.0)
+        fAxis = np.linspace(0,fMax/2,SAMPLEPOINTS/2)
         plt.plot(fAxis,FFTmeasurepoints)
         plt.xlabel("f in kHz")
         plt.ylabel("FFT")
         plt.xlim([min(fAxis),max(fAxis)])
         plt.xlim([0,1.25])
-
+        
+        
         #THD is calculated
-       
+        
         THD = 0 #Startwert der THD
-        for i in range(2,40):
+        for i in range(2,40): #ist bisher falsch
             freqency_guess = 10
             THD_Amplituden = np.max(FFTmeasurepoints[(freqency_guess*i-5):(freqency_guess*i+5)])    #max. amplitude nehmen im Bereich der harmonischen Schwingungen (muss noch angepasst werden)           
             THD_Amplituden = np.power(THD_Amplituden/(230*np.sqrt(2)),2)  #4000 just an example -> 4000 must be replaced with Un
@@ -79,32 +99,22 @@ if __name__ == '__main__':
         print("THD = " + str(THD))
         
         #rms voltage is calculated
-
-        time1 = time.time()
-        rms = 0.0   #Startwert des rms
-        for i in range(N): #Effektivwert über N Messwerte
-            rms = measurepoints[i]**2+rms
-         
-        rms = np.sqrt(rms/N) 
-        time2 = time.time()
-        print("rms = " + str(rms)) #braucht für viele messwerte exponetiell länger
-        
-#        rms2 = np.absolute(measurepoints)
-#        rms2 = np.power(measurepoints,2)
-#        
-#        rms2 = math.fsum(rms2)
-#        
-#        rms2 = np.sqrt(rms2/N)
-#        time3 = time.time()
-#        print("rms2 = " + str(rms2))
-#        print("Time_for = " + str(time2-time1))
-#        print("Time_sum = " + str(time3-time2))
-    
+        rms = calculate_rms(measurement)
+        print("Effektivwert = " + str(rms))
         #freqency is calculated
         
-        
-    
-    #except:
+#        for i in xrange(SAMPLEPOINTSPYLESAMPLEPOINTSGTH):
+#            if measurepoints[i]<0 & measurepoints[i+1]>0 & measurepoints[i+1]<measurepoints[i+2]:
+#                period_counter = 1                
+#                period_begin = i + np.abs(measurepoints[i])/(measurepoints[i+1] + np.abs(measurepoints[i]))           
+#                period_counter += 1
+#                print(period_begin)
+#                
+#            else:
+#                print("neuer durchlauf")
+            
+
+    except:
         print("there must be something wrong")
     
     finally:
