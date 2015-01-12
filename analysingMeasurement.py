26e81f7c935ee30de216da72ce33c03e60c9c258
kanell <maltegerber@gmail.com>
2015-01-12 15:51:06 +0100

    Cleanup und speedtest

--- analysingMeasurement.py
+++ analysingMeasurement.py
@@ -11,9 +11,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 import scipy.signal as signal
+import time
+import flicker as fl
 
-
-SAMPLEPOINTS = 220000   #Samplepoints
+SAMPLEPOINTS = 2**18#220000   #Samplepoints
 SAMPLING_RATE = 1000000  #Sampling Rate
 Un = 230 #Volt
 
@@ -21,9 +22,9 @@ Un = 230 #Volt
 def load_measurepoints(SAMPLEPOINTS):
     #Load files from the directory        
     npyfiles = glob.glob('C:\\Users\Malte Gerber\Desktop\Messdaten\*.npy')
-    measurepoints = np.load(npyfiles[20])
+    measurepoints = np.load(npyfiles[0])
     #add the content from all files to one list
-    i = 20
+    i = 0
     while len(measurepoints) < SAMPLEPOINTS:
         #Messdaten werden solange in ein Array geladen, bis es mehr als die vorgegebene Samplepoints sind        
         measurepoints = np.concatenate([measurepoints, (np.load(npyfiles[i]))], axis=0) 
@@ -33,28 +34,28 @@ def load_measurepoints(SAMPLEPOINTS):
     return measurepoints
     
 def fast_fourier_transformation(SAMPLEPOINTS, SAMPLING_RATE):
-    
-    plot_FFT = 0    #Show FFT Signal Plot    
-    
+    plot_FFT = 0    #Show FFT Signal Plot        
     #Laden der Messwerte    
     measurepoints = load_measurepoints(SAMPLEPOINTS)
     #berechnen der Fouriertransformation        
     FFTmeasurepoints = np.fft.fftshift(np.fft.fft(measurepoints))/SAMPLEPOINTS 
+              
+    #FFTphase = np.angle(FFTmeasurepoints)    
     #Frequenzen der Oberschwingungen    
-    FFTfrequency = np.fft.fftfreq(len(FFTmeasurepoints), 1.0/SAMPLING_RATE)
+    FFTfrequencys = np.fft.fftfreq(len(FFTmeasurepoints), 1.0/SAMPLING_RATE)
     #wegkürzen der negativen frequenzen und dafür verdoppeln der Amplituden
     FFTmeasurepoints = np.abs(FFTmeasurepoints[(len(FFTmeasurepoints)/2):])*2
     
     if (plot_FFT):
-        deltaT = 1.0/(SAMPLING_RATE)   #s ->Zeit von einem Messpunkt zum nächsten
-        fMax = 1.0/deltaT   #Hz -> Maximale Frequenz der Fourieranalyse
-        fAxis = np.linspace(0,fMax/2,SAMPLEPOINTS/2) #x-Achse von 0 bis nyquist-frequenz=fmax/2 mit SAMPLEPOINTS/2 Datenpunkten
-        plt.plot(fAxis,FFTmeasurepoints) #Plot der Messwerte der FFT über die gegebene x-Achse fMax
+        #deltaT = 1.0/(SAMPLING_RATE)   #s ->Zeit von einem Messpunkt zum nächsten
+        #fMax = 1.0/deltaT   #Hz -> Maximale Frequenz der Fourieranalyse
+        #fAxis = np.linspace(0,fMax/2,SAMPLEPOINTS/2) #x-Achse von 0 bis nyquist-frequenz=fmax/2 mit SAMPLEPOINTS/2 Datenpunkten
+        plt.plot(FFTfrequencys[:SAMPLEPOINTS/2], FFTmeasurepoints) #Plot der Messwerte der FFT über die gegebene x-Achse fMax
         plt.xlabel("f in Hz") # y-Achse beschriefen
         plt.ylabel("FFT") # x-Achse beschriften
         plt.xlim([0,1500]) # länge der angezeigten x-Achse
     
-    return FFTmeasurepoints, FFTfrequency  
+    return FFTmeasurepoints, FFTfrequencys  
     
 def calculate_rms():
     #Laden der Messwerte
@@ -69,39 +70,38 @@ def example_sin_wave(SAMPLEPOINTS, SAMPLING_RATE):  #Beispiel für Sinusschwingu
     measurepoints = 230*np.sqrt(2)*np.sin(2*np.pi/Tn*t)+2*np.sqrt(2)*np.sin(2*np.pi/Tn*3*t)+5*np.sqrt(2)*np.sin(2*np.pi/Tn*5*t)+6*np.sqrt(2)*np.sin(2*np.pi/Tn*7*t)+4*np.sqrt(2)*np.sin(2*np.pi/Tn*9*t)+2*np.sqrt(2)*np.sin(2*np.pi/Tn*11*t) #Array mit Messwerten
     return measurepoints
     
-def THD(SAMPLEPOINTS, SAMPLING_RATE):
+def calculate_THD_KlasseA(SAMPLEPOINTS, SAMPLING_RATE):
     #Lade Messpunkte der FFT
-    FFTmeasurepoints = fast_fourier_transformation(SAMPLEPOINTS, SAMPLING_RATE)[0]
+    FFTmeasurepoints, FFTfrequencys = fast_fourier_transformation(SAMPLEPOINTS, SAMPLING_RATE)
     THD = 0 #Startwert der THD
+    Bereich_Amplituden = round(SAMPLEPOINTS/float(SAMPLING_RATE)*calculate_Frequency(SAMPLING_RATE)) #an dieser Stelle sollte sich der Amplitudenausschlag der Oberschwingung befinden           
     for i in range(2,41): 
-        Bereich_Amplituden = round(SAMPLEPOINTS/float(SAMPLING_RATE)/0.02*i) #an dieser Stelle sollte sich der Amplitudenausschlag der Oberschwingung befinden           
-        THD_Amplituden = FFTmeasurepoints[Bereich_Amplituden] #direkter Amplitudenwert aus FFT   
-    #Berechnung des THD-Wertes über eine for-Schleife        
+        THD_Amplituden = FFTmeasurepoints[round(Bereich_Amplituden*i)] #direkter Amplitudenwert aus FFT   
+        #Berechnung des THD-Wertes über eine for-Schleife        
         THD_Amplituden = np.power((THD_Amplituden),2)  
         THD = THD + THD_Amplituden
-    THD = np.sqrt(THD/(FFTmeasurepoints[SAMPLEPOINTS/float(SAMPLING_RATE)/0.02]**2)) #darf nicht größer als 8% betragen
-    return THD
+    THD_KlasseA = np.sqrt(THD/(FFTmeasurepoints[SAMPLEPOINTS/float(SAMPLING_RATE)/0.02]**2)) #darf nicht größer als 8% betragen
+    return THD_KlasseA
 
-def Oberschwingungen_Gruppierung(SAMPLEPOINTS, SAMPLING_RATE):
+def calculate_THD_KlasseS(SAMPLEPOINTS, SAMPLING_RATE):
     #Lade Messpunkte der FFT   
-    FFTmeasurepoints = fast_fourier_transformation(SAMPLEPOINTS, SAMPLING_RATE)
+    FFTmeasurepoints, FFTfrequencys= fast_fourier_transformation(SAMPLEPOINTS, SAMPLING_RATE)
     Gruppierung = 0
+    Bereich_Amplituden = int(round(SAMPLEPOINTS/float(SAMPLING_RATE)*calculate_Frequency(SAMPLING_RATE))) #an dieser Stelle sollte sich der Amplitudenausschlag der Oberschwingung befinden 
     for i in range(1,41): 
-        Bereich_Amplituden = SAMPLEPOINTS/float(SAMPLING_RATE)/0.02*i #an dieser Stelle sollte sich der Amplitudenausschlag der Oberschwingung befinden           
-        Gruppierung_teil1 = 0.5*FFTmeasurepoints[Bereich_Amplituden-(SAMPLEPOINTS/float(SAMPLING_RATE)/0.02)/2]**2
-        Gruppierung_teil2 = 0.5*FFTmeasurepoints[Bereich_Amplituden+(SAMPLEPOINTS/float(SAMPLING_RATE)/0.02)/2]**2
-        FFTmeasurepoints_Bereich = FFTmeasurepoints[(Bereich_Amplituden-(SAMPLEPOINTS/float(SAMPLING_RATE)/0.02)/2+1):(Bereich_Amplituden+(SAMPLEPOINTS/float(SAMPLING_RATE)/0.02)/2)]**2       
+        Gruppierung_teil1 = 0.5*FFTmeasurepoints[int(round(Bereich_Amplituden*i-Bereich_Amplituden/2))]**2
+        Gruppierung_teil2 = 0.5*FFTmeasurepoints[int(round(Bereich_Amplituden*i+Bereich_Amplituden/2))]**2
+        FFTmeasurepoints_Bereich = FFTmeasurepoints[int(round(Bereich_Amplituden*i-Bereich_Amplituden/2+1)):int(round(Bereich_Amplituden+Bereich_Amplituden/2)-1)]**2       
         Gruppierung_teil3 = np.sum(FFTmeasurepoints_Bereich)
         Gruppierung = Gruppierung_teil1+Gruppierung_teil2+Gruppierung_teil3+Gruppierung
         if (i==1):
             erste_Oberschwingung = Gruppierung
             Gruppierung = 0
-    THD_Gruppierung = np.sqrt(Gruppierung/erste_Oberschwingung)
-    return THD_Gruppierung
+    THD_KlasseS = np.sqrt(Gruppierung/erste_Oberschwingung)
+    return THD_KlasseS
 
 def calculate_Frequency(SAMPLING_RATE):    
     measurepoints_filtered = Hamming_Filter()    
-    
     t = []       
     for i in range(len(measurepoints_filtered)-1):
         if (measurepoints_filtered[i]<=0 and measurepoints_filtered[i+1]>0):           
@@ -118,15 +118,11 @@ def calculate_Frequency(SAMPLING_RATE):
     return freqency_seconds
     
 def Hamming_Filter():
-    
     show_filtered_measurement = 0    
-        
     #Lade Messwerte
-    #measurepoints = load_measurepoints(SAMPLEPOINTS)
-    measurepoints = example_sin_wave(SAMPLEPOINTS, SAMPLING_RATE)    
+    measurepoints = load_measurepoints(SAMPLEPOINTS)    
     #Filterkern wird erstellt als Rechteck-Funktion mit 30 Stützstellen und einem cutoff von 0.008    
     kern = signal.firwin(5, cutoff = 0.008, window = "hamming")
-    
     #Messdaten werden gefiltert
     measurepoints_filtered = signal.lfilter(kern, 1, measurepoints)
     
@@ -140,24 +136,35 @@ def Hamming_Filter():
     
 if __name__ == '__main__':
     try:
-        
+        time1 = time.time()
         #THD is calculated
-        THD = THD(SAMPLEPOINTS, SAMPLING_RATE)
-        print("THD = " + str(THD))
-#        THD_Gruppierung = Oberschwingungen_Gruppierung(SAMPLEPOINTS, SAMPLING_RATE)
-#        print("THD_Gruppierung = " + str(THD_Gruppierung))
-        
+        THD_KlasseA = calculate_THD_KlasseA(SAMPLEPOINTS, SAMPLING_RATE)
+        print("THD nach Klasse A = " + str(THD_KlasseA))
+        time2 = time.time()        
+        THD_KlasseS = calculate_THD_KlasseS(SAMPLEPOINTS, SAMPLING_RATE)
+        print("THD nach Klasse S = " + str(THD_KlasseS))
+        time3 = time.time()
         #rms voltage is calculated
-#        rms = calculate_rms()
-#        print("Effektivwert = " + str(rms))
-        
+        rms = calculate_rms()
+        print("Effektivwert = " + str(rms))
+        time4 = time.time()
         #freqency is calculated
         freqency = calculate_Frequency(SAMPLING_RATE)
         print("Frequenz = " + str(freqency))
+        time5 = time.time()
+        Pst = fl.calculate_Pst()
+        print("Pst = " + str(Pst))
+        time6 = time.time()
         
+        print("THD-Berechnungszeit (Klasse A) = " + str(time2-time1) + " sekunden")
+        print("THD-Berechnungszeit (Klasse S) = " + str(time3-time2) + " sekunden")
+        print("Effektivwert-Berechnungzeit = " + str(time4-time3) + " sekunden")
+        print("Frequenzberechnungszeit = " + str(time5-time4) + " sekunden")
+        print("Flickerberechnungszeit = " + str(time6-time5) + " sekunden")
+        print("Gesamtberechnungszeit = " + str(time6-time1) + " sekunden")
 
-    except:
+    #except:
         print("there must be something wrong")
     
     finally:
-        print("the analysis has been completed")
+        print("Alle Daten wurden berechnet")
--- flicker.py
+++ flicker.py
@@ -37,159 +37,162 @@ show_filter_responses = 0       #Aktivierung des Plots der Amplitudengänge der 
 f_line = 50
 fs = 4000
 
-u = np.load('testwerte1.npy')
-#u = ana.example_sin_wave(2400000, 4000)
-
-## Block 1: Modulierung des Spannungssignals
-
-u = u - np.mean(u)                      # entfernt DC-Anteil
-u_rms = np.sqrt(np.mean(np.power(u,2))) # Normierung des Eingangssignals
-u = u / (u_rms * np.sqrt(2))
-
-## Block 2: Quadratischer Demulator
-
-u_0 = u**2
-
-## Block 3: Hochpass-, Tiefpass- und Gewichtungsfilter
-
-# Konfiguration der Filter
-HIGHPASS_ORDER  = 1 #Ordnungszahl der Hochpassfilters
-HIGHPASS_CUTOFF = 0.05 #Hz Grenzfrequenz
-
-LOWPASS_ORDER = 6 #Ordnungszahl des Tiefpassfilters
-if (f_line == 50):
-  LOWPASS_CUTOFF = 35 #Hz Grenzfrequenz
-
-if (f_line == 60):
-  LOWPASS_CUTOFF = 42 #Hz Grenzfrequenz
-
-# subtract DC component to limit filter transients at start of simulation
-u_0_ac = u_0 - np.mean(u_0)
-
-b_hp, a_hp = signal.butter(HIGHPASS_ORDER, (HIGHPASS_CUTOFF/(fs/2)), 'highpass')
-u_hp = signal.lfilter(b_hp, a_hp, u_0_ac)
-
-# smooth start of signal to avoid filter transient at start of simulation
-smooth_limit = min(round(fs / 10), len(u_hp))
-u_hp[ : smooth_limit] = u_hp[ : smooth_limit] * np.linspace(0, 1, smooth_limit)
-
-b_bw, a_bw = signal.butter(LOWPASS_ORDER, (LOWPASS_CUTOFF/(fs/2)), 'lowpass')
-u_bw = signal.lfilter(b_bw, a_bw, u_hp)
-
-# Gewichtungsfilter (Werte sind aus der Norm)
-
-if (f_line == 50):
-  K = 1.74802
-  LAMBDA = 2 * np.pi * 4.05981
-  OMEGA1 = 2 * np.pi * 9.15494
-  OMEGA2 = 2 * np.pi * 2.27979
-  OMEGA3 = 2 * np.pi * 1.22535
-  OMEGA4 = 2 * np.pi * 21.9
-
-if (f_line == 60):
-  K = 1.6357
-  LAMBDA = 2 * np.pi * 4.167375
-  OMEGA1 = 2 * np.pi * 9.077169
-  OMEGA2 = 2 * np.pi * 2.939902
-  OMEGA3 = 2 * np.pi * 1.394468
-  OMEGA4 = 2 * np.pi * 17.31512
-
-num1 = [K * OMEGA1, 0]
-denum1 = [1, 2 * LAMBDA, OMEGA1**2]
-num2 = [1 / OMEGA2, 1]
-denum2 = [1 / (OMEGA3 * OMEGA4), 1 / OMEGA3 + 1 / OMEGA4, 1]
-
-b_w, a_w = signal.bilinear(np.convolve(num1, num2), np.convolve(denum1, denum2), fs)
-u_w = signal.lfilter(b_w, a_w, u_bw)
-
-## Block 4: Quadrierung und Varianzschätzer
-
-LOWPASS_2_ORDER  = 1
-LOWPASS_2_CUTOFF = 1 / (2 * np.pi * 300e-3)  # Zeitkonstante 300 msek.
-SCALING_FACTOR   = 1238400  # Skalierung des Signals auf eine Wahrnehmbarkeitsskala
-
-u_q = u_w**2
-
-b_lp, a_lp = signal.butter(LOWPASS_2_ORDER, (LOWPASS_2_CUTOFF/(fs/2)), 'low')
-s = SCALING_FACTOR * signal.lfilter(b_lp, a_lp, u_q)
-
-## Block 5: Statistische Berechnung
-
-p_50s = np.mean([np.percentile(s, 100-30, interpolation="linear"),
-                 np.percentile(s, 100-50, interpolation="linear"),
-                 np.percentile(s, 100-80, interpolation="linear")])
-p_10s = np.mean([np.percentile(s, 100-6, interpolation="linear"),
-                 np.percentile(s, 100-8, interpolation="linear"), 
-                 np.percentile(s, 100-10, interpolation="linear"),
-                 np.percentile(s, 100-13, interpolation="linear"),
-                 np.percentile(s, 100-17, interpolation="linear")])
-p_3s = np.mean([np.percentile(s, 100-2.2, interpolation="linear"),
-                np.percentile(s, 100-3, interpolation="linear"),
-                np.percentile(s, 100-4, interpolation="linear")])
-p_1s = np.mean([np.percentile(s, 100-0.7, interpolation="linear"),
-                np.percentile(s, 100-1, interpolation="linear"),
-                np.percentile(s, 100-1.5, interpolation="linear")])
-p_0_1s = np.percentile(s, 100-0.1, interpolation="linear")
-
-P_st = np.sqrt(0.0314*p_0_1s+0.0525*p_1s+0.0657*p_3s+0.28*p_10s+0.08*p_50s)
-print("Pst = " + str(P_st))
-
-time2 = time.time()
-print("Benötigte Zeit in Sekunden = " + str(time2-time1))
-
-if (show_time_signals):
-    t = np.linspace(0, len(u) / fs, num=len(u))
-    plt.figure()
-    plt.clf()
-    #plt.subplot(2, 2, 1)
-    plt.hold(True)
-    plt.plot(t, u, 'b', label="u")
-    plt.plot(t, u_0, 'm', label="u_0")
-    plt.plot(t, u_hp, 'r', label="u_hp")
-    plt.xlim(0, len(u)/fs)
-    plt.hold(False)
-    plt.legend(loc=1)
-    plt.grid(True)
-    #plt.subplot(2, 2, 2)
-    plt.figure()
-    plt.clf()    
-    plt.hold(True)
-    plt.plot(t, u_bw, 'b', label="u_bw")
-    plt.plot(t, u_w, 'm', label="u_w")
-    plt.xlim(0, len(u)/fs)
-    plt.legend(loc=1)
-    plt.hold(False)
-    plt.grid(True)
-    #plt.subplot(2, 2, 3)
-    plt.figure()
-    plt.clf()    
-    plt.plot(t, u_q, 'b', label="u_q")
-    plt.xlim(0, len(u)/fs)
-    plt.legend(loc=1)
-    plt.grid(True)
-    #plt.subplot(2, 2, 4)
-    plt.figure()
-    plt.clf()    
-    plt.plot(t, s, 'b', label="s")
-    plt.xlim(0, len(u)/fs)
-    plt.legend(loc=1)
-    plt.grid(True)
-
-if (show_filter_responses):
-    f, h_hp = signal.freqz(b_hp, a_hp, 4096)
-    f, h_bw = signal.freqz(b_bw, a_bw, 4096)
-    f, h_w = signal.freqz(b_w, a_w, 4096)
-    f, h_lp = signal.freqz(b_lp, a_lp, 4096)
-    f = f/np.pi*fs/2    
+def calculate_Pst():    
+    u = np.load('testwerte1.npy')
+    #u = ana.example_sin_wave(2400000, 4000)
     
-    plt.figure()
-    plt.clf()
-    plt.hold(True)
-    plt.plot(f, abs(h_hp), 'b', label="Hochpass 1. Ordnung")
-    plt.plot(f, abs(h_bw), 'r', label="Butterworth Tiefpass 6.Ordnung")
-    plt.plot(f, abs(h_w), 'g', label="Gewichtungsfilter")
-    plt.plot(f, abs(h_lp), 'm', label="Varianzschätzer")
-    plt.legend(bbox_to_anchor=(1., 1.), loc=2)    
-    plt.hold(False)
-    plt.grid(True)
-    plt.axis([0, 35, 0, 1])
+    ## Block 1: Modulierung des Spannungssignals
+    
+    u = u - np.mean(u)                      # entfernt DC-Anteil
+    u_rms = np.sqrt(np.mean(np.power(u,2))) # Normierung des Eingangssignals
+    u = u / (u_rms * np.sqrt(2))
+    
+    ## Block 2: Quadratischer Demulator
+    
+    u_0 = u**2
+    
+    ## Block 3: Hochpass-, Tiefpass- und Gewichtungsfilter
+    
+    # Konfiguration der Filter
+    HIGHPASS_ORDER  = 1 #Ordnungszahl der Hochpassfilters
+    HIGHPASS_CUTOFF = 0.05 #Hz Grenzfrequenz
+    
+    LOWPASS_ORDER = 6 #Ordnungszahl des Tiefpassfilters
+    if (f_line == 50):
+      LOWPASS_CUTOFF = 35 #Hz Grenzfrequenz
+    
+    if (f_line == 60):
+      LOWPASS_CUTOFF = 42 #Hz Grenzfrequenz
+    
+    # subtract DC component to limit filter transients at start of simulation
+    u_0_ac = u_0 - np.mean(u_0)
+    
+    b_hp, a_hp = signal.butter(HIGHPASS_ORDER, (HIGHPASS_CUTOFF/(fs/2)), 'highpass')
+    u_hp = signal.lfilter(b_hp, a_hp, u_0_ac)
+    
+    # smooth start of signal to avoid filter transient at start of simulation
+    smooth_limit = min(round(fs / 10), len(u_hp))
+    u_hp[ : smooth_limit] = u_hp[ : smooth_limit] * np.linspace(0, 1, smooth_limit)
+    
+    b_bw, a_bw = signal.butter(LOWPASS_ORDER, (LOWPASS_CUTOFF/(fs/2)), 'lowpass')
+    u_bw = signal.lfilter(b_bw, a_bw, u_hp)
+    
+    # Gewichtungsfilter (Werte sind aus der Norm)
+    
+    if (f_line == 50):
+      K = 1.74802
+      LAMBDA = 2 * np.pi * 4.05981
+      OMEGA1 = 2 * np.pi * 9.15494
+      OMEGA2 = 2 * np.pi * 2.27979
+      OMEGA3 = 2 * np.pi * 1.22535
+      OMEGA4 = 2 * np.pi * 21.9
+    
+    if (f_line == 60):
+      K = 1.6357
+      LAMBDA = 2 * np.pi * 4.167375
+      OMEGA1 = 2 * np.pi * 9.077169
+      OMEGA2 = 2 * np.pi * 2.939902
+      OMEGA3 = 2 * np.pi * 1.394468
+      OMEGA4 = 2 * np.pi * 17.31512
+    
+    num1 = [K * OMEGA1, 0]
+    denum1 = [1, 2 * LAMBDA, OMEGA1**2]
+    num2 = [1 / OMEGA2, 1]
+    denum2 = [1 / (OMEGA3 * OMEGA4), 1 / OMEGA3 + 1 / OMEGA4, 1]
+    
+    b_w, a_w = signal.bilinear(np.convolve(num1, num2), np.convolve(denum1, denum2), fs)
+    u_w = signal.lfilter(b_w, a_w, u_bw)
+    
+    ## Block 4: Quadrierung und Varianzschätzer
+    
+    LOWPASS_2_ORDER  = 1
+    LOWPASS_2_CUTOFF = 1 / (2 * np.pi * 300e-3)  # Zeitkonstante 300 msek.
+    SCALING_FACTOR   = 1238400  # Skalierung des Signals auf eine Wahrnehmbarkeitsskala
+    
+    u_q = u_w**2
+    
+    b_lp, a_lp = signal.butter(LOWPASS_2_ORDER, (LOWPASS_2_CUTOFF/(fs/2)), 'low')
+    s = SCALING_FACTOR * signal.lfilter(b_lp, a_lp, u_q)
+    
+    ## Block 5: Statistische Berechnung
+    
+    p_50s = np.mean([np.percentile(s, 100-30, interpolation="linear"),
+                     np.percentile(s, 100-50, interpolation="linear"),
+                     np.percentile(s, 100-80, interpolation="linear")])
+    p_10s = np.mean([np.percentile(s, 100-6, interpolation="linear"),
+                     np.percentile(s, 100-8, interpolation="linear"), 
+                     np.percentile(s, 100-10, interpolation="linear"),
+                     np.percentile(s, 100-13, interpolation="linear"),
+                     np.percentile(s, 100-17, interpolation="linear")])
+    p_3s = np.mean([np.percentile(s, 100-2.2, interpolation="linear"),
+                    np.percentile(s, 100-3, interpolation="linear"),
+                    np.percentile(s, 100-4, interpolation="linear")])
+    p_1s = np.mean([np.percentile(s, 100-0.7, interpolation="linear"),
+                    np.percentile(s, 100-1, interpolation="linear"),
+                    np.percentile(s, 100-1.5, interpolation="linear")])
+    p_0_1s = np.percentile(s, 100-0.1, interpolation="linear")
+    
+    P_st = np.sqrt(0.0314*p_0_1s+0.0525*p_1s+0.0657*p_3s+0.28*p_10s+0.08*p_50s)
+    
+    
+    #time2 = time.time()
+    #print("Benötigte Zeit in Sekunden = " + str(time2-time1))
+    
+    if (show_time_signals):
+        t = np.linspace(0, len(u) / fs, num=len(u))
+        plt.figure()
+        plt.clf()
+        #plt.subplot(2, 2, 1)
+        plt.hold(True)
+        plt.plot(t, u, 'b', label="u")
+        plt.plot(t, u_0, 'm', label="u_0")
+        plt.plot(t, u_hp, 'r', label="u_hp")
+        plt.xlim(0, len(u)/fs)
+        plt.hold(False)
+        plt.legend(loc=1)
+        plt.grid(True)
+        #plt.subplot(2, 2, 2)
+        plt.figure()
+        plt.clf()    
+        plt.hold(True)
+        plt.plot(t, u_bw, 'b', label="u_bw")
+        plt.plot(t, u_w, 'm', label="u_w")
+        plt.xlim(0, len(u)/fs)
+        plt.legend(loc=1)
+        plt.hold(False)
+        plt.grid(True)
+        #plt.subplot(2, 2, 3)
+        plt.figure()
+        plt.clf()    
+        plt.plot(t, u_q, 'b', label="u_q")
+        plt.xlim(0, len(u)/fs)
+        plt.legend(loc=1)
+        plt.grid(True)
+        #plt.subplot(2, 2, 4)
+        plt.figure()
+        plt.clf()    
+        plt.plot(t, s, 'b', label="s")
+        plt.xlim(0, len(u)/fs)
+        plt.legend(loc=1)
+        plt.grid(True)
+    
+    if (show_filter_responses):
+        f, h_hp = signal.freqz(b_hp, a_hp, 4096)
+        f, h_bw = signal.freqz(b_bw, a_bw, 4096)
+        f, h_w = signal.freqz(b_w, a_w, 4096)
+        f, h_lp = signal.freqz(b_lp, a_lp, 4096)
+        f = f/np.pi*fs/2    
+        
+        plt.figure()
+        plt.clf()
+        plt.hold(True)
+        plt.plot(f, abs(h_hp), 'b', label="Hochpass 1. Ordnung")
+        plt.plot(f, abs(h_bw), 'r', label="Butterworth Tiefpass 6.Ordnung")
+        plt.plot(f, abs(h_w), 'g', label="Gewichtungsfilter")
+        plt.plot(f, abs(h_lp), 'm', label="Varianzschätzer")
+        plt.legend(bbox_to_anchor=(1., 1.), loc=2)    
+        plt.hold(False)
+        plt.grid(True)
+        plt.axis([0, 35, 0, 1])
+        
+        return P_st
