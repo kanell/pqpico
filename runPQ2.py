364a28300fbace09ffc511a94a72f0228bcde7a4
kipfer <christoph.kattmann@gmail.com>
2015-02-12 18:27:28 +0100

    10 second separation

--- runPQ2.py
+++ runPQ2.py
@@ -8,8 +8,9 @@ pico = Picoscope4000.Picoscope4000()
 pico.run_streaming()
 
 parameters = pico.get_parameters()
+streaming_sample_interval = parameters['streaming_sample_interval']
 
-min_snippet_length = parameters['streaming_sample_interval']/2
+min_snippet_length = streaming_sample_interval/2
 
 data = np.array([])
 is_first_iteration = 1
@@ -37,7 +38,8 @@ try:
             time.sleep(0.1)
 
         data_chunk = data[:min_snippet_length]
-        zero_indices = pq.detect_zero_crossings(data_chunk ,parameters['streaming_sample_interval'])
+        data_chunk = data_chunk - np.mean(data_chunk)
+        zero_indices = pq.detect_zero_crossings(data,parameters['streaming_sample_interval'])
         print('data_chunk: '+str(data_chunk))
         print('zero_indices: '+str(zero_indices))
         print(np.diff(zero_indices))   
@@ -47,6 +49,17 @@ try:
         #data_10periods = data(zero_indices[10])
         #np.save('/home/kipfer/pqpico/Data/data_chunk',data_chunk) 
         print(len(data_chunk))
+        data_10periods = data[:zero_indices[20]-1]
+        data_10seconds = np.append(data_10seconds, data_10periods) #Preallocation?
+        data = data[zero_indices[20]:]
+        shift_zeros = zero_indices - zero_indices[0]
+        #for i in xrange(20):    
+            #rms_half_period = pq.calculate_rms_half_period(data_10periods[shift_zeros[i]:shift_zeros[i+1]])
+        rms_10_periods = pq.calculate_rms(data_10periods)
+        #harmonics = pq.calculate_harmonics_ClassA(data_10periods, parameters['streaming_sample_interval'])
+        if data_10seconds.size >= streaming_sample_interval*10:
+            freq_data = data_10seconds[:streaming_sample_interval*10]
+            data_10seconds = data_10seconds[streaming_sample_interval+1:]
 
 finally:
     pico.close_unit()
